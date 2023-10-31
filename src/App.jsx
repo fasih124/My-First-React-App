35e7de3b3df3209838c431f2c21e4c25ef444634
fasih <fasih124124@gmail.com>
2023-10-31 21:24:25 +0500

    update basicdfdsfsdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Note from "./component/Note";
--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+
 function Note(props){
     return (
         <div className="note">
             <h2>{props.heading}</h2>
             <p>{props.description}</p>
+            <button>DELETE</button>
         </div>
     );
 }
