1c472b17b88bca7930a633e6f393815dcbfef212
fasih <fasih124124@gmail.com>
2023-10-31 21:26:27 +0500

    fixed typo and comment un-nessecary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Note from "./component/Note";
-import notes from "./notes";
+import CreateArea from "./component/CreateArea";
+// import notes from "./notes";
 
 function App() {
   return (
     <>
       <Header />
-      {notes.map((noteEntry) => (
+      <CreateArea />
+      {
+      /* {notes.map((noteEntry) => (
         <Note
           key={noteEntry.key}
           heading={noteEntry.title}
           description={noteEntry.content}
         />
-      ))}
+      ))} */
+      }
+      <Note key={1} title="Note title" content="Note content" />
+
       <Footer />
     </>
   );
